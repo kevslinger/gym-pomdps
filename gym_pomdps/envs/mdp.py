8ae13fba66beb16ee38035d89412facc437c224c
kevslinger <siouxman@me.com>
2020-11-03 12:30:19 -0500

    added cheese mdp class

--- gym_pomdps/envs/mdp.py
+++ gym_pomdps/envs/mdp.py
@@ -6,7 +6,7 @@ from gym.utils import seeding
 
 from rl_parsers.mdp import parse
 
-__all__ = ['MDP', 'HallwayMDP', 'MITMDP']
+__all__ = ['MDP', 'HallwayMDP', 'MITMDP', 'ShoppingMDP', 'CheeseMDP']
 
 # NOTE: THIS is currently only for the hallways domain...
 class MDP(gym.Env):  # pylint: disable=abstract-method
@@ -27,6 +27,9 @@ class MDP(gym.Env):  # pylint: disable=abstract-method
         self.observation_space = None
         self.goal = None
         self.reward_range = model.R.min(), model.R.max()
+
+        self.steps = 0
+        self.step_cap = 0
 
         self.rewards_dict = {r: i for i, r in enumerate(np.unique(model.R))}
 
@@ -62,9 +65,9 @@ class MDP(gym.Env):  # pylint: disable=abstract-method
         obs = self.get_state()
         achieved_goal = self.get_state()
         return {
-            'observation' : obs,
-            'achieved_goal' : achieved_goal,
-            'desired_goal' : self.goal
+            'observation': obs,
+            'achieved_goal': achieved_goal,
+            'desired_goal': self.goal
         }
 
     # any state between 44 and 59 can be a goal (4 orientations in one of the 4 boxes.)
@@ -80,6 +83,7 @@ class MDP(gym.Env):  # pylint: disable=abstract-method
 
     def reset(self):
         self.state = self.reset_functional()
+        self.steps = 0
         return self._get_obs()
 
     def step(self, action):
@@ -134,11 +138,15 @@ class MDP(gym.Env):  # pylint: disable=abstract-method
         if done:
             state_next = -1
 
+        self.steps += 1
+        if self.steps >= self.step_cap:
+            done = True
+
         # reward_cat = self.rewards_dict[reward]
         # info = dict(reward_cat=reward_cat)
 
         # return state_next, obs, reward, done, info
-        return (state_next, reward, done, info)
+        return state_next, reward, done, info
 
     def compute_reward(self, achieved_goal, goal, info):
         if achieved_goal == goal:
@@ -163,6 +171,7 @@ class HallwayMDP(MDP):
             achieved_goal=spaces.Discrete(len(self.model.states)),
             observation=spaces.Discrete(len(self.model.states)),
         ))
+        self.step_cap = 15
 
     # any state between 44 and 59 can be a goal (4 orientations in one of the 4 boxes.)
     def _sample_goal(self):
@@ -186,6 +195,26 @@ class MITMDP(MDP):
 
 class ShoppingMDP(MDP):
     def __init__(self, text, *, episodic, seed=None):
-        super().__init__(text, episodic=episodic, seed=seed):
+        super().__init__(text, episodic=episodic, seed=seed)
+
         self.goal = self._sample_goal()
-        self.observation_space = spaces.Dict(dict)
+        self.observation_space = spaces.Dict(dict(
+            desired_goal=spaces.Discrete(len(self.model.states))
+        ))
+
+
+class CheeseMDP(MDP):
+    def __init__(self, text, *, episodic, seed=None):
+        super().__init__(text, episodic=episodic, seed=seed)
+
+        self.goal = self._sample_goal()
+        self.observation_space = spaces.Dict(dict(
+            desired_goal=spaces.Discrete(3),
+            achieved_goal=spaces.Discrete(len(self.model.states)),
+            observation=spaces.Discrete(len(self.model.states)),
+        ))
+        self.step_cap = 10
+
+    # only states 9, 10, and 11 can be goals for now
+    def _sample_goal(self):
+        return np.random.choice([8, 9, 10])
