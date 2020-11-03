da644848507ec39a347ecb36d77b8b63fb0ac41f
kevslinger <tug74186@temple.edu>
2020-11-03 17:51:46 -0500

    change log dir for sparse rewards

--- scratch.py
+++ scratch.py
@@ -15,15 +15,15 @@ def main(args):
     if args.env == 'hallway':
         env = gym.make('MDP-hallwaymdp-episodic-v0')
         model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-                    tensorboard_log='./logs/hallway/', verbose=1)
+                    tensorboard_log='./logs/hallway_sparse/', verbose=1)
     elif args.env == 'mit':
         env = gym.make('MDP-mitmdp-episodic-v0')
         model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-                    tensorboard_log='./logs/mit/', verbose=1)
+                    tensorboard_log='./logs/mit_sparse/', verbose=1)
     elif args.env == 'cheese':
         env = gym.make('MDP-cheesemdp-episodic-v0')
         model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-                tensorboard_log='./logs/cheese/', verbose=1)
+                tensorboard_log='./logs/cheese_sparse/', verbose=1)
     else:
         raise NotImplementedError('Environment not yet implemented. Current environment are [\'cheese\', \'mit\', and \'hallway\']')
     model.learn(200000)
