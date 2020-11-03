3081ba8ef288de9bd9478eed70bd8b20c5d54c0a
kevslinger <siouxman@me.com>
2020-11-03 11:06:09 -0500

    moved MDP class from pomdp.py to its own file

--- scratch.py
+++ scratch.py
@@ -10,9 +10,10 @@ import gym, gym_pomdps
 
 model_class = DQN
 goal_selection_strategy = 'future'
+#env = gym.make('MDP-hallwaymdp-episodic-v0')
 env = gym.make('MDP-mitmdp-episodic-v0')
 
 model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-            tensorboard_log='./logs/', verbose=1)
+            tensorboard_log='./logs/mit/', verbose=1)
 
 model.learn(1000000)
