e070e480f8104ee5ff68c13465f9903e7d045feb
kevslinger <siouxman@me.com>
2020-11-03 12:30:34 -0500

    now have the ability to change environment at command line

--- scratch.py
+++ scratch.py
@@ -1,5 +1,5 @@
 
-
+import argparse
 import tensorflow as tf
 import numpy as np
 
@@ -8,12 +8,31 @@ from stable_baselines.her import GoalSelectionStrategy, HERGoalEnvWrapper
 #from stable_baselines.common.callbacks import BaseCallbacks
 import gym, gym_pomdps
 
-model_class = DQN
-goal_selection_strategy = 'future'
-#env = gym.make('MDP-hallwaymdp-episodic-v0')
-env = gym.make('MDP-mitmdp-episodic-v0')
 
-model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-            tensorboard_log='./logs/mit/', verbose=1)
+def main(args):
+    model_class = DQN
+    goal_selection_strategy = 'future'
+    if args.env == 'hallway':
+        env = gym.make('MDP-hallwaymdp-episodic-v0')
+        model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
+                    tensorboard_log='./logs/hallway/', verbose=1)
+    elif args.env == 'mit':
+        env = gym.make('MDP-mitmdp-episodic-v0')
+        model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
+                    tensorboard_log='./logs/mit/', verbose=1)
+    elif args.env == 'cheese':
+        env = gym.make('MDP-cheesemdp-episodic-v0')
+        model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
+                tensorboard_log='./logs/cheese/', verbose=1)
+    else:
+        raise NotImplementedError('Environment not yet implemented. Current environment are [\'cheese\', \'mit\', and \'hallway\']')
+    model.learn(1000000)
 
-model.learn(1000000)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env')
+
+    main(parser.parse_args())
