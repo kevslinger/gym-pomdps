c73993c6bbda9e5f51f6e2f3a5a33c7b736a6861
kevslinger <tug74186@temple.edu>
2020-11-03 08:53:23 -0500

    added a file for testing

---
+++ scratch.py
@@ -0,0 +1,18 @@
+
+
+import tensorflow as tf
+import numpy as np
+
+from stable_baselines import HER, DQN
+from stable_baselines.her import GoalSelectionStrategy, HERGoalEnvWrapper
+#from stable_baselines.common.callbacks import BaseCallbacks
+import gym, gym_pomdps
+
+model_class = DQN
+goal_selection_strategy = 'future'
+env = gym.make('MDP-mitmdp-episodic-v0')
+
+model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
+            tensorboard_log='./logs/', verbose=1)
+
+model.learn(1000000)
