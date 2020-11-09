a2f250e49854f70e6f25f4c70a2ed65f49829b73
kevslinger <tug74186@temple.edu>
2020-11-09 10:48:51 -0500

    add learn to cheese one hot

--- scratch.py
+++ scratch.py
@@ -32,7 +32,7 @@ def main(args):
         env = gym.make('MDP-cheeseonehotmdp-episodic-v0')
         model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=args.goal_selection_strategy,
                     tensorboard_log='./logs/cheeseonehot', verbose=1)
-
+        model.learn(200000)
 
 
 
