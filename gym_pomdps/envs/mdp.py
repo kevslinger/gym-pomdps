6d7b2ccf9f821ee1614e8ea2d1ee4cec90eb6bcd
kevslinger <tug74186@temple.edu>
2020-11-10 15:20:14 -0500

    hallway is capped

--- gym_pomdps/envs/mdp.py
+++ gym_pomdps/envs/mdp.py
@@ -171,7 +171,7 @@ class HallwayMDP(MDP):
             achieved_goal=spaces.Discrete(len(self.model.states)),
             observation=spaces.Discrete(len(self.model.states)),
         ))
-        self.step_cap = 15  # np.inf
+        self.step_cap = np.inf  # 15
 
     # any state between 44 and 59 can be a goal (4 orientations in one of the 4 boxes.)
     def _sample_goal(self):
