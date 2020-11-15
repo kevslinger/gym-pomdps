7bb9f663c2733218526153efdbf78550bba0eec4
kevslinger <tug74186@temple.edu>
2020-11-14 23:06:24 -0500

    add generic logdir as an arg

--- scratch.py
+++ scratch.py
@@ -58,6 +58,8 @@ if __name__ == '__main__':
                         help='Goal selection strategy (choose \'future\' or \'final\')')
     parser.add_argument('--layer-size', type=int, default=64,
                         help='Size of DQN architecture (e.g. [64, 64], [32, 32], ...')
+    parser.add_argument('--logdir', type=str, default='./logs',
+                        help='Where to store logs')
 
 
     main(parser.parse_args())
