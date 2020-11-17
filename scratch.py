56397f4efbec1d17c4ca9952a67e995c32f7a05a
kevslinger <tug74186@temple.edu>
2020-11-17 13:48:33 -0500

    finally fixed the registration bug for int environments

--- scratch.py
+++ scratch.py
@@ -11,7 +11,7 @@ import gym, gym_pomdps
 
 def main(args):
     model_class = DQN
-    env = gym.make('MDP-' + args.env + '-episodic-v0')
+    env = gym.make('MDP-' + args.env + 'mdp-episodic-v0')
     model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=args.goal_selection_strategy,
                 tensorboard_log=args.logdir, verbose=1)
     model.learn(200000)
