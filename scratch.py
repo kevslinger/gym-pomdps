82cee3a2c8ec3a6ada9de4de7d3a55cd9fa344b9
kevslinger <tug74186@temple.edu>
2020-11-29 14:13:08 -0500

    adding seeds

--- scratch.py
+++ scratch.py
@@ -5,19 +5,11 @@ import numpy as np
 
 from stable_baselines import HER, DQN
 from stable_baselines.her import GoalSelectionStrategy, HERGoalEnvWrapper
-#from stable_baselines.common.callbacks import BaseCallbacks
-import gym, gym_pomdps
+import gym
+import gym_pomdps
 
 
-def main(args):
-    model_class = DQN
-    env = gym.make('MDP-' + args.env + 'mdp-episodic-v0')
-    model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=args.goal_selection_strategy,
-                tensorboard_log=args.logdir, verbose=1)
-    model.learn(200000)
-
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', help='Environment you want to process')
     parser.add_argument('--goal_selection_strategy', type=str, default='future',
@@ -26,6 +18,16 @@ if __name__ == '__main__':
                         help='Size of DQN architecture (e.g. [64, 64], [32, 32], ...')
     parser.add_argument('--logdir', type=str, default='./logs',
                         help='Where to store logs')
+    parser.add_argument('--seed', type=int,
+                        help='Random seed')
+    args = parser.parse_args()
+
+    model_class = DQN
+    env = gym.make('MDP-' + args.env + 'mdp-episodic-v0')
+    model = HER('MlpPolicy', env, model_class, n_sampled_goal=4, goal_selection_strategy=args.goal_selection_strategy,
+                seed=args.seed, tensorboard_log=args.logdir, verbose=1)
+    model.learn(200000)
 
 
-    main(parser.parse_args())
+if __name__ == '__main__':
+    main()
